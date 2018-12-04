6aed9af734e696b85a17e92f993fe632288d7c41
osskari <oskarjorgen@gmail.com>
2018-12-04 16:16:12 +0000

    Created entity classes with getters and setters

---
+++ app/src/main/java/a/b/c/quizmania/Entities/Score.java
@@ -0,0 +1,59 @@
+package a.b.c.quizmania.Entities;
+
+public class Score {
+
+    private String difficulty;
+    private int category;
+    private String mode;
+    private int totalQuestions;
+    private int correctAnswers;
+
+    public Score(String difficulty, int category, String mode,
+                 int totalQuestions, int correctAnswers) {
+        this.difficulty = difficulty;
+        this.category = category;
+        this.mode = mode;
+        this.totalQuestions = totalQuestions;
+        this.correctAnswers = correctAnswers;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public int getTotalQuestions() {
+        return totalQuestions;
+    }
+
+    public void setTotalQuestions(int totalQuestions) {
+        this.totalQuestions = totalQuestions;
+    }
+
+    public int getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public void setCorrectAnswers(int correctAnswers) {
+        this.correctAnswers = correctAnswers;
+    }
+}
