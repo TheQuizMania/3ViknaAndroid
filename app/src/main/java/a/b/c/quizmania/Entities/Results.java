8839caa80eb08b474e61087af634e40afe35c4aa
osskari <oskarjorgen@gmail.com>
2018-12-04 17:24:16 +0000

    created GSON entities

---
+++ app/src/main/java/a/b/c/quizmania/Entities/Results.java
@@ -0,0 +1,59 @@
+package a.b.c.quizmania.Entities;
+
+public class Results {
+    private String category;
+    private String type;
+    private String question;
+    private String correctAnswer;
+    private String[] incorrectAnswers;
+
+    public Results(String category, String type,
+                   String question, String correctAnswer,
+                   String[] incorrectAnswers) {
+        this.category = category;
+        this.type = type;
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.incorrectAnswers = incorrectAnswers;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String[] getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
+    public void setIncorrectAnswers(String[] incorrectAnswers) {
+        this.incorrectAnswers = incorrectAnswers;
+    }
+}
