5201416bf904f2dff5efa22fbbb48a3b87792bda
helloyello34 <kristinn34@gmail.com>
2018-12-05 23:12:44 +0000

    some minor changes

--- app/src/main/java/a/b/c/quizmania/UI/QuestionActivity.java
+++ app/src/main/java/a/b/c/quizmania/UI/QuestionActivity.java
@@ -29,7 +29,4 @@ public class QuestionActivity extends AppCompatActivity {
 
 
     }
-
-
-
 }
--- app/src/main/java/a/b/c/quizmania/UI/RegisterActivity.java
+++ app/src/main/java/a/b/c/quizmania/UI/RegisterActivity.java
@@ -1,11 +1,15 @@
 package a.b.c.quizmania.UI;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
