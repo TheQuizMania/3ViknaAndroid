7f7d193edc8ce107298b69c74f2a9fc779329bac
helloyello34 <kristinn34@gmail.com>
2018-12-05 14:12:56 +0000

    Dropdown implemented

--- app/src/main/java/a/b/c/quizmania/UI/SelectionActivity.java
+++ app/src/main/java/a/b/c/quizmania/UI/SelectionActivity.java
@@ -2,12 +2,29 @@ package a.b.c.quizmania.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import a.b.c.quizmania.R;
 
-public class SelectionActivity extends AppCompatActivity {
+public class SelectionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    Spinner categoryDropDown;
 
+    String[] categories = {
+            "Entertainment Random",
+            "Science Random",
+            "General Knowledge",
+            "Sports",
+            "History & Mythology",
+            "Politics",
+            "Geography",
+            "Video Games",
+            "Television & Film",
+            "Music"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +32,27 @@ public class SelectionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_selection);
         getSupportActionBar().hide();
 
+
+        // Find Views
+        categoryDropDown = (Spinner)findViewById(R.id.category_dropdown);
+
+        // Listeners
+        categoryDropDown.setOnItemSelectedListener(this);
+
+        ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, categories);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        categoryDropDown.setAdapter(aa);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(),categories[position] , Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
