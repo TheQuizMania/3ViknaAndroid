7b8db829ef107a8b24495b95fcca56e63ba76a5d
helloyello34 <kristinn34@gmail.com>
2018-12-12 14:39:11 +0000

    moved calls to FirebaseDatabase to Utility.java

--- app/src/main/java/a/b/c/quizmania/db/Utility.java
+++ app/src/main/java/a/b/c/quizmania/db/Utility.java
@@ -3,6 +3,10 @@ package a.b.c.quizmania.db;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Objects;
+
+import a.b.c.quizmania.Entities.Challenge;
+import a.b.c.quizmania.Entities.Score;
 import a.b.c.quizmania.Entities.UserListItem;
 
 public class Utility {
@@ -22,5 +26,29 @@ public class Utility {
                 .child(FirebaseAuth.getInstance().getUid())
                 .setValue(newUser);
     }
+	public static void addScore(Score score) {
+		String uId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+		FirebaseDatabase.getInstance().getReference().child("root")
+				.child("Users")
+				.child(uId)
+				.child("scores")
+				.push()
+				.setValue(score);
+	}
+
+	public static void addChallenge(Challenge challenge) {
+		FirebaseDatabase.getInstance().getReference().child("root")
+				.child("challenges")
+				.push()
+				.setValue(challenge);
+	}
+
+	public static void updateChallenge(Challenge challenge) {
+		FirebaseDatabase.getInstance().getReference().child("root")
+				.child("challenges")
+				.child(challenge.getId())
+				.setValue(challenge);
+	}
+
 
 }
